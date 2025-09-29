646d8c032ae96d6716660fd3eaccb4a4f5a1dbb6
dofla2910 <nineoxon2005@gmail.com>
2025-09-29 19:38:43 +0700

    fix import error

--- src/User/pages/paymentcancel.jsx
+++ src/User/pages/paymentcancel.jsx
@@ -1,10 +1,9 @@
-import { useNavigate } from "react-router-dom";
-import DefaultButton from "../../components/DefaultButton";
-import "../css/paymentcancel.css";
-import DefaultButton from "../../components/DefaultButton";
+import { useNavigate } from 'react-router-dom'
+import DefaultButton from '../../components/DefaultButton'
+import '../css/paymentcancel.css'
 
 const PaymentCancel = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   // useEffect(() => {
   //     // Quay lại trang trước đó
@@ -12,12 +11,11 @@ const PaymentCancel = () => {
   // }, [navigate]);
 
   return (
-    <div className="payment-cancel-wrapper">
-      <CircleCheckIcon className="payment-cancel-icon" />
-      <h2 className="payment-cancel-header">Payment Canceled</h2>
-      <p className="payment-cancel-text">
-        Your payment has been canceled. If you have any questions, please
-        contact our support team.
+    <div className='payment-cancel-wrapper'>
+      <CircleCheckIcon className='payment-cancel-icon' />
+      <h2 className='payment-cancel-header'>Payment Canceled</h2>
+      <p className='payment-cancel-text'>
+        Your payment has been canceled. If you have any questions, please contact our support team.
       </p>
       {/* <Button
                 className="back-to-homepage-button"
@@ -27,38 +25,34 @@ const PaymentCancel = () => {
             >
                 Back to Homepage
             </Button> */}
-      <DefaultButton
-        type="primary"
-        style={{ marginTop: "20px" }}
-        onClick={() => navigate("/user/homepage")}
-      >
+      <DefaultButton type='primary' style={{ marginTop: '20px' }} onClick={() => navigate('/user/homepage')}>
         Back to Homepage
       </DefaultButton>
     </div>
-  );
-};
+  )
+}
 
 function CircleCheckIcon(props) {
   return (
     <svg
       {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      aria-hidden="true"
-      color="red"
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      aria-hidden='true'
+      color='red'
     >
-      <circle cx="12" cy="12" r="10" />
-      <path d="m15 9-6 6" />
-      <path d="m9 9 6 6" />
+      <circle cx='12' cy='12' r='10' />
+      <path d='m15 9-6 6' />
+      <path d='m9 9 6 6' />
     </svg>
-  );
+  )
 }
 
-export default PaymentCancel;
+export default PaymentCancel
